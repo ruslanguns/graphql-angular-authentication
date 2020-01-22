b3fea66a81467f3e972dc9b418bf80400ba067e1
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-01-22 14:04:48 +0000

    Instalación básica de Graphql y ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import gql from 'graphql-tag';
+import { Apollo } from 'apollo-angular';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graphql-angular-authentication';
+
+  constructor(
+    private apollo: Apollo
+  ) { }
+
+  ngOnInit() {
+    const getUsers = gql`
+
+    query {
+      users {
+        id
+        name
+        lastname
+        email
+        registerDate
+      }
+    }
+    `;
+
+    this.apollo
+      .watchQuery(
+        {
+          query: getUsers,
+          fetchPolicy: 'network-only'
+        }
+      ).valueChanges.pipe(
+        map((result: any) => {
+          return result.data.users;
+        }),
+      ).subscribe(console.log);
+  }
 }
